faa8b3bcabd1865f63fe86265be6708556584ee3
Andy Tu <andysctu@gmail.com>
2015-12-16 19:03:24 -0800

    hello world

---
+++ game.py
@@ -0,0 +1,26 @@
+import pyglet 
+from pyglet.window import key
+
+window = pyglet.window.Window()
+label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == key.A:
+        print 'The "A" key was pressed.'
+    elif symbol == key.LEFT:
+        print 'The left arrow key was pressed.'
+    elif symbol == key.ENTER:
+        print 'The enter key was pressed.'
+
+window.push_handlers(pyglet.window.event.WindowEventLogger())
+
+pyglet.app.run()
